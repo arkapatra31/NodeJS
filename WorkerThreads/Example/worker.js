9f540b99bfe4be8227b7fce7974849e505a54ae7
Arka <arkapatra2009@gmail.com>
2022-11-22 19:29:34 +0530

    Doc

--- WorkerThreads/Example/worker.js
+++ WorkerThreads/Example/worker.js
@@ -1,3 +1,7 @@
+//Worker Threads in Node.js is useful for performing heavy JavaScript tasks. 
+//With the help of threads, Worker makes it easy to run javascript codes in parallel making it much faster and efficient. 
+//We can do heavy tasks without even disturbing the main thread. 
+
 const { workerData, parentPort } = require("worker_threads");
 
 console.log("Technical Articles on " + workerData);
